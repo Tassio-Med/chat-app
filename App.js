fd682f05b00f901a27ae6a5e14203501e94de74c
Tassio-Med <tassiohfmed@gmail.como>
2023-03-25 12:05:18 -0300

    :sparkles: feat: substituindo importação do Chat por Login.

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Chat from './screens/Chat';
+import Login from './screens/Login'
 
 const Stack = createStackNavigator();
 
 function ChatStack () {
   return (
     <Stack.Navigator>
-      <Stack.Screen  name="Chat" component={Chat} />
+      <Stack.Screen  name="Login" component={Login} />
     </Stack.Navigator>
   )
 }
