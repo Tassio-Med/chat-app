83bb51bc66756cdc0f62e14a5232cdf21e5ca543
Tassio-Med <tassiohfmed@gmail.como>
2023-03-25 12:43:23 -0300

    :sparkles: feat: função de autenticação de usuário adicionada.

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { onAuthStateChanged } from "firebase/auth";
 
 import Chat from './screens/Chat';
-import Login from './screens/Login'
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import Home from './screens/Home';
 
 const Stack = createStackNavigator();
+const AuthenticatedUserContext = createContext({});
+
+const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <AuthenticatedUserContext.Provider value={{user, setUser}}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  )
+}
 
 function ChatStack () {
   return (
-    <Stack.Navigator>
-      <Stack.Screen  name="Login" component={Login} />
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen  name="SignUp" component={Signup} />
     </Stack.Navigator>
   )
 }
