a904aae12dcd1c85cf754b72da63689aa1ef4a26
Tassio-Med <tassiohfmed@gmail.como>
2023-03-25 15:15:00 -0300

    :sparkles: feat: importações iniciais para desenvolvimento do chat

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,5 +1,23 @@
-import React from 'react'
+import React, {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useCallback
+} from 'react'
+import { TouchableOpacity, Text } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat'
+import {
+  collection,
+  addDoc,
+  orderBy,
+  query,
+  onSnapshot
+} from 'firebase/firestore';
+import { signOut } from 'firebase/auth';
+import { auth, database } from '../config/firebase';
+import { useNavigation } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
+import colors from '../color';
 
 export default function Chat() {
   return (
