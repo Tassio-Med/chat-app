21ed222afaaa2aa47aa48a0d958133259174637e
Tassio-Med <tassiohfmed@gmail.como>
2023-03-25 15:16:50 -0300

    :sparkles: feat: funções para conexão com o banco de dados e envio de mensagens.

--- screens/Chat.js
+++ screens/Chat.js
@@ -20,6 +20,63 @@ import { AntDesign } from '@expo/vector-icons';
 import colors from '../color';
 
 export default function Chat() {
+
+  const [messages, setMessages] = useState([]);
+  const navigation = useNavigation();
+
+const onSignOut = () => {
+    signOut(auth).catch(error => console.log('Error logging out: ', error));
+  };
+
+  useLayoutEffect(() => {
+      navigation.setOptions({
+        headerRight: () => (
+          <TouchableOpacity
+            style={{
+              marginRight: 10
+            }}
+            onPress={onSignOut}
+          >
+            <AntDesign name="logout" size={24} color={colors.gray} style={{marginRight: 10}}/>
+          </TouchableOpacity>
+        )
+      });
+    }, [navigation]);
+
+  useLayoutEffect(() => {
+
+      const collectionRef = collection(database, 'chats');
+      const q = query(collectionRef, orderBy('createdAt', 'desc'));
+
+  const unsubscribe = onSnapshot(q, querySnapshot => {
+      console.log('querySnapshot unsusbscribe');
+        setMessages(
+          querySnapshot.docs.map(doc => ({
+            _id: doc.data()._id,
+            createdAt: doc.data().createdAt.toDate(),
+            text: doc.data().text,
+            user: doc.data().user
+          }))
+        );
+      });
+  return unsubscribe;
+    }, []);
+
+  const onSend = useCallback((messages = []) => {
+      setMessages(previousMessages =>
+        GiftedChat.append(previousMessages, messages)
+      );
+      // setMessages([...messages, ...messages]);
+      const { _id, createdAt, text, user } = messages[0];    
+      addDoc(collection(database, 'chats'), {
+        _id,
+        createdAt,
+        text,
+        user
+      });
+    }, []);
+
+
   return (
     <GiftedChat />
   )
